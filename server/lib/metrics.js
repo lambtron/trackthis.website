63c6a04e7a8df2f7c58d150281419b0c0d9d759b
Andy Jiang <andyjiang@gmail.com>
2015-05-25 15:15:57 -0700

    updated server/lib/metrics.js

--- server/lib/metrics.js
+++ server/lib/metrics.js
@@ -5,21 +5,35 @@
 
 var db = require('./db');
 var wrap = require('co-monk');
-var Metrics = wrap(db.get('metrics'));
+
+/**
+ * Instantiate new Metrics object.
+ */
+
+function Metrics(name) {
+  if (!(this instanceof Metrics)) return new Metrics(name);
+  this.db = wrap(db.get(name));
+  this.name = name;
+
+  /**
+   * Increment.
+   */
+
+  this.increment = function *(key) {
+    var query = {};
+    query[this.name] = key; // referrals: google.com
+    var res = yield this.db.findOne(query);
+    if (!res) {
+      query.count = 1;
+      return yield this.db.insert(query);
+    }
+    res.count++;
+    return yield this.db.update(res);
+  };
+}
 
 /**
  * Expose `Metrics`.
  */
 
 module.exports = Metrics;
-
-// TODO: figure out the interface of this
-//
-// live stats
-// - referrer breakdown
-// - average time spent on site (or histogram)
-// - useragent breakdown
-// - ip address breakdown?
-// - total hits
-// - total unique hits
-// - hits over time?
