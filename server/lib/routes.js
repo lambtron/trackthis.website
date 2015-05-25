3f2de596d6313f8e17c516f1fa5dff359c78aace
Andy Jiang <andyjiang@gmail.com>
2015-05-25 15:15:57 -0700

    updated server/lib/routes.js

--- server/lib/routes.js
+++ server/lib/routes.js
@@ -3,6 +3,8 @@
  * Module dependencies.
  */
 
+var Session = require('./session');
+var Events = require('./events');
 var render = require('./render');
 
 /**
@@ -10,15 +12,33 @@ var render = require('./render');
  */
 
 exports.index = function *() {
-  return this.body = yield render('index');
+  // var metrics = yield Events.get();
+  metrics = {
+    referrers: [{ referrer: 'google.com', count: 10 }, { referrer: 'facebook.com', count: 5 }],
+    sessions: [{ session: 30, count: 10 }, { session: 60, count: 2 }],
+    agents: [{ agent: 'chrome', count: 10 }, { agent: 'firefox', count: 2 }]
+  };
+  return this.body = yield render('index', metrics);
+};
+
+// {
+//   referrers: [{ 'google.com': 10 }, { 'facebook.com': 5 }],
+//   sessions: [{ 30: 10 }, { 60: 2 }],
+//   agents: [{ 'chrome': 10 }, { 'firefox': 2 }]
+// }
+
+/**
+ * API route for receiving event.
+ */
+
+exports.event = function *() {
+  return yield Events.add(this.request.body);
 };
 
 /**
- * API route for receiving ping.
+ * API route for receiving session.
  */
 
-exports.ping = function *() {
-  var user = this.request.body;
-  console.log(user);
-  return this.body = 200;
+exports.session = function *() {
+  return yield Session.add(this.request.body);
 };
